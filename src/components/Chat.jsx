cf5d8a88497b34ab969bb90d4169364e9a972e4c
kiritoast <jinming625@gmail.com>
2023-05-06 15:21:30 -0700

    finish up the ui

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Cam from "../img/cam.png"
+import Add from "../img/add.png"
+import More from "../img/more.png"
+import Messages from './Messages'
+import Input from './Input'
 
 const Chat = () => {
   return (
@@ -6,11 +11,14 @@ const Chat = () => {
       <div className="chatInfo">
          <span>Jane</span>
          <div className="chatIcons">
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" /> 
+          <img src={Cam}  alt="" />
+          <img src={Add} alt="" /> 
+          <img src={More} alt="" />  
          </div>
       </div>
+      <Messages/>
+      <Input />
+
     </div>
   )
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2wFbn76E19sWf_yUQMbzXHS4DcRf1KG19mQ&usqp=CAU" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2wFbn76E19sWf_yUQMbzXHS4DcRf1KG19mQ&usqp=CAU" alt="" />
+      </div>
+    </div>
   )
 }
 
