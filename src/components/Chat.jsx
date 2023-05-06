0d89839be0fcb82d99474a95774f1c63e94340ef
kiritoast <jinming625@gmail.com>
2023-05-06 13:30:03 -0700

    adding side bar for users

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+         <span>Jane</span>
+         <div className="chatIcons">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" /> 
+         </div>
+      </div>
+    </div>
   )
 }
 
-export default Chat
+export default Chat 
